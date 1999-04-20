f8cdb73ab28f1bab08c512c0662d282ca06f19ec
webdav <webdav@e9a49f44-22ea-0310-85f4-17a4d2cea23e>
1999-04-20 23:05:10 +0000

    Changed backslash to forward slash

--- trunk/classes/DAVjar.sh
+++ trunk/classes/DAVjar.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jar -cfm DAVExplorer.jar DAVManifest DAVExplorer\*.class DAVExplorer\icons\* HTTPClient\*.class HTTPClient\http\*.class HTTPClient\https\*.class HTTPClient\shttp\*.class com\ms\xml\dso\*.class com\ms\xml\om\*.class com\ms\xml\parser\*.class com\ms\xml\util\*.class com\ms\xml\xmlstream\*.class
+jar -cfm DAVExplorer.jar DAVManifest DAVExplorer/*.class DAVExplorer/icons/* HTTPClient/*.class HTTPClient/http/*.class HTTPClient/https/*.class HTTPClient/shttp/*.class com/ms/xml/dso/*.class com/ms/xml/om/*.class com/ms/xml/parser/*.class com/ms/xml/util/*.class com/ms/xml/xmlstream/*.class
