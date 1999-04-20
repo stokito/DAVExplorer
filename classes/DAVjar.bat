dcd9a60c974da80d25f80c61ad450bba3dd272b1
webdav <webdav@e9a49f44-22ea-0310-85f4-17a4d2cea23e>
1999-04-20 01:50:11 +0000

    Shell scripts and installation procedure

---
+++ classes/DAVjar.bat
@@ -0,0 +1,2 @@
+rem @echo off
+jar -cfm DAVExplorer.jar DAVManifest DAVExplorer\*.class DAVExplorer\icons\* HTTPClient\*.class HTTPClient\http\*.class HTTPClient\https\*.class HTTPClient\shttp\*.class com\ms\xml\dso\*.class com\ms\xml\om\*.class com\ms\xml\parser\*.class com\ms\xml\util\*.class com\ms\xml\xmlstream\*.class
