105acc4173fac7d8b1038defed95266bf8662a2a
webdav <webdav@e9a49f44-22ea-0310-85f4-17a4d2cea23e>
1999-05-01 02:31:37 +0000

    Batch file updates.

--- classes/make.bat
+++ classes/make.bat
@@ -1,2 +1,13 @@
 @echo off
+set OLDCLASSPATH=%CLASSPATH%
+for /f %%i in ('cd') do set CLASSPATH=%%i;%CLASSPATH%
+cd com
+call make.bat
+cd ..\HTTPClient
+call make.bat
+cd ..\DAVExplorer
+call make.bat
+echo on
+cd ..
 jar -cfm DAVExplorer.jar DAVManifest DAVExplorer\*.class DAVExplorer\icons\* HTTPClient\*.class HTTPClient\http\*.class HTTPClient\https\*.class HTTPClient\shttp\*.class com\ms\xml\dso\*.class com\ms\xml\om\*.class com\ms\xml\parser\*.class com\ms\xml\util\*.class com\ms\xml\xmlstream\*.class
+set CLASSPATH=%OLDCLASSPATH%
