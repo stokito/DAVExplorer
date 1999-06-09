d932aa70ea4a9253e9300b38d3e7db7f11cee00a
cvswrite <cvswrite@e9a49f44-22ea-0310-85f4-17a4d2cea23e>
1999-06-09 02:58:29 +0000

    fixed slashes in command

--- DAVjar.sh
+++ DAVjar.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd classes; jar -cfm DAVExplorer.jar DAVManifest DAVExplorer\*.class DAVExplorer\icons\* HTTPClient\*.class HTTPClient\http\*.class HTTPClient\https\*.class HTTPClient\shttp\*.class com\ms\xml\dso\*.class com\ms\xml\om\*.class com\ms\xml\parser\*.class com\ms\xml\util\*.class com\ms\xml\xmlstream\*.class
+cd classes; jar -cfm DAVExplorer.jar DAVManifest DAVExplorer/*.class DAVExplorer/icons/* HTTPClient/*.class HTTPClient/http/*.class HTTPClient/https/*.class HTTPClient/shttp/*.class com/ms/xml/dso/*.class com/ms/xml/om/*.class com/ms/xml/parser/*.class com/ms/xml/util/*.class com/ms/xml/xmlstream/*.class
