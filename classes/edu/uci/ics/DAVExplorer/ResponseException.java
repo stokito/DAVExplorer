6ae993699a7c49ccedf71442323863b1005972b8
cvswrite <cvswrite@e9a49f44-22ea-0310-85f4-17a4d2cea23e>
2000-06-21 00:01:36 +0000

    Improved handling of server timeouts

---
+++ classes/edu/uci/ics/DAVExplorer/ResponseException.java
@@ -0,0 +1,16 @@
+package DAVExplorer;
+
+public class ResponseException extends Exception
+{
+
+    public ResponseException()
+    {
+        super();
+    }
+
+    public ResponseException( String msg )
+    {
+	    super( msg );
+    }
+}
+
