41d1a8326e402bdb4e78772c7305a30bf815a75e
cvswrite <cvswrite@e9a49f44-22ea-0310-85f4-17a4d2cea23e>
2001-05-30 01:44:03 +0000

    bumped the version number

--- trunk/classes/edu/uci/ics/DAVExplorer/Main.java
+++ trunk/classes/edu/uci/ics/DAVExplorer/Main.java
@@ -84,7 +84,7 @@ public class Main extends JFrame
     WebDAVMenu CommandMenu;
     Hashtable authTable;
     String authHost;
-    public final static String VERSION = "0.71-dev";
+    public final static String VERSION = "0.72";
     public final static String UserAgent = "UCI DAV Explorer/" + VERSION;
     String writeToDir;
 
